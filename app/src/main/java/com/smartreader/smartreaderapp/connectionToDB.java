1d2c1aebc490dba03352549dc25f8c92b281ffca
beautiful-mess <30566980+beautiful-mess@users.noreply.github.com>
2018-02-28 18:56:31 +0300

    Initial commit

---
+++ app/src/main/java/com/smartreader/smartreaderapp/connectionToDB.java
@@ -0,0 +1,18 @@
+package com.smartreader.smartreaderapp;
+
+/**
+ * Created by Mejo on 2/23/2018.
+ */
+
+public class connectionToDB {
+    public connectionToDB() {
+
+    }
+    public static void main(String [ ] args){
+
+
+
+    }
+}
+
+
